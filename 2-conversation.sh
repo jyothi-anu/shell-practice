7852fbb66c6e8e2a7e2d53bd53a6edcd76fab9ff
jyothi <jyothi3626@gmail.com>
2026-01-28 16:52:11 +0530

    removd.sh

--- 2-conversation.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#---conversation between two persons---
-echo "ramesh::how are you suresh?"
-echo "suresh:: im fine thankyou! how about you"
-echo "ramesh::im doing great suresh what shall we meet up ramesh!"
-echo "suresh:: im already on the way to meet you ramesh!"
--- 3-variable.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#--declaring variable and assigning values
-PERSON1=$1;PERSON2=$2
-echo "$PERSON1::how are you $PERSON2?"
-echo "$PERSON2:: im fine thankyou! how about you"
-echo "$PERSON1::im doing great $PERSON2 what shall we meet up $PERSON1!"
-echo "$PERSON2:: im already on the way to meet you $PERSON1!"
