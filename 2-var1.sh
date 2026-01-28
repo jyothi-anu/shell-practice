b20dc281eeb04f8ea121364027f29f934c03b70f
jyothi <jyothi3626@gmail.com>
2026-01-28 12:31:24 +0530

    printing messages

---
+++ 2-var1.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# understanding the variable storage
+echo "hi suresh how are u?"
+echo "im fine ramesh thank you"
+echo "its nice talking to you suresh"
+echo "when are you going to meet ramesh"
+echo "im very much excited to meet you suresh said by ramesh"
