5f557fa41906a908d8c8d2e4018a00f489de00ec
jyothi <jyothi3626@gmail.com>
2026-01-28 14:39:17 +0530

    stopped tracking 2-var*.sh

--- 2-var1.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# understanding the variable storage
-name1="suresh"
-name2="ramesh"
-echo "hi $name1 how are u?"
-echo "im fine $name2 thank you"
-echo "its nice talking to you $name1"
-echo "when are you going to meet $name2"
-echo "im very much excited to meet you $name1 said by $name2"
