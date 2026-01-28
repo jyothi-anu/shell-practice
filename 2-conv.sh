ea4e67addf77163eb642466d992e408e559bd4b2
jyothi <jyothi3626@gmail.com>
2026-01-28 17:27:25 +0530

    ADD VAR

---
+++ 2-conv.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#===========conversion message============
+PERSON1="ramesh"
+PERSON2="SURESH"
+echo "$PERSON1:: hi how are you $PERSON2"
+echo "$PERSON2:: hello im fine $PERSON1 how about u"
+echo "$PERSON1:: im doinf great! shall we meet today"
+echo "$PERSON2:: yes ofcourse $PERSON1 im already on the way"
