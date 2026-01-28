450ec3c9062e99d72244ee257e21d84ed8212545
jyothi <jyothi3626@gmail.com>
2026-01-28 13:17:46 +0530

    breaking echo into new lines

--- 2-var2.sh
+++ 2-var2.sh
@@ -4,5 +4,5 @@ name1="jyothi";number=25;course="aws and devops"
 echo "$name1 of age$number taking $course"
 #**********************************************************************
 #alwys use capital for environment variabls
-echo -e "show me current directory $PWD\n and $PATH\n and $UNAME\n"
+echo -e "show me current directory $PWD\n and $UNAME\n"
 #***********************************************************************
