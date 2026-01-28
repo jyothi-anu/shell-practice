75e2fcb095da99a3544fc13968c6cd47a24dd04c
jyothi <jyothi3626@gmail.com>
2026-01-28 17:34:25 +0530

    ADD VAR

--- 3-var.sh
+++ 3-var.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #creating variable and assigning value to it
-name="jyothi";age=25
-echo -e "my name=$name\n age=$age"
+name="jyothi";age=25;course="devops"
+echo -e "my name=$name\n age=$age\n course=$course"
