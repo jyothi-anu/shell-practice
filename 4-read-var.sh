89258b9b56df8c3b53a5eea94c0caa32a0382cde
jyothi <jyothi3626@gmail.com>
2026-01-28 15:50:06 +0530

    chngs done

---
+++ 4-read-var.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#taking user detaisl druing runtime
+echo "ENTER NAME"
+read USER_NAME
+echo "user name:" $USER_NAME
+echo "ENTER PASSWORD"
+read -s PASSWORD
+echo "password is:"$PASSWORD
