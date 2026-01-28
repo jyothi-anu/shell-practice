8b4c7e8a80ba262e177289347e3f1e491a88b7d9
jyothi <jyothi3626@gmail.com>
2026-01-28 17:36:28 +0530

    ADD VAR

---
+++ 4-runtime_var.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi how are you $PERSON2"
+echo "$PERSON2:: hello im fine $PERSON1 how about u"
+echo "$PERSON1:: im doinf great! shall we meet today"
+echo "$PERSON2:: yes ofcourse $PERSON1 im already on the way"
