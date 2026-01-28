10215d36cb258e1075e2e7826f2b7b80d987210d
jyothi <jyothi3626@gmail.com>
2026-01-28 13:07:20 +0530

    $ changed captial leter to environ var

--- 2-var2.sh
+++ 2-var2.sh
@@ -2,4 +2,7 @@
 #To declare variable and assigning values to it
 name1="jyothi";number=25;course="aws and devops"
 echo "$name1 of age$number taking $course"
-echo "show me current directory$pwd and $path and $uname"
+**********************************************************************
+#alwys use capital for environment variabls
+echo "show me current directory$PWD and $PATH and $UNAME"
+***********************************************************************
