507a1dc9eba50ef61dea3e706c0680aed29aa314
jyothi <jyothi3626@gmail.com>
2026-01-28 15:28:23 +0530

    chngs done

---
+++ 3-variable.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#--declaring variable and assigning values
+PERSON1="rama";PERSON2="laxman"
+echo "$PERSON1::how are you $PERSON2?"
+echo "$PERSON2:: im fine thankyou! how about you"
+echo "$PERSON1::im doing great $PERSON2 what shall we meet up $PERSON1!"
+echo "$PERSON2:: im already on the way to meet you $PERSON1!"
