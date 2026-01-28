e39b2e03d6c75d0a3fa141e41354ae501bba84fe
jyothi <jyothi3626@gmail.com>
2026-01-28 12:41:27 +0530

    missed to place $ for variable name refering on echo stmt

--- 2-var1.sh
+++ 2-var1.sh
@@ -2,8 +2,8 @@
 # understanding the variable storage
 name1 = "suresh"
 name2 = "ramesh"
-echo "hi name1 how are u?"
-echo "im fine name2 thank you"
-echo "its nice talking to you name1"
-echo "when are you going to meet name2"
-echo "im very much excited to meet you name1 said by name2"
+echo "hi $name1 how are u?"
+echo "im fine $name2 thank you"
+echo "its nice talking to you $name1"
+echo "when are you going to meet $name2"
+echo "im very much excited to meet you name1 said by $name2"
