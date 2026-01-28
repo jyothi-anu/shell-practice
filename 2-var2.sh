a85263ef5538db41afe6a04a4b87fed3843c6160
jyothi <jyothi3626@gmail.com>
2026-01-28 13:33:25 +0530

    something

--- 2-var2.sh
+++ 2-var2.sh
@@ -2,7 +2,9 @@
 #To declare variable and assigning values to it
 name1="jyothi";number=25;course="aws and devops"
 echo "$name1 of age$number taking $course"
-#**********************************************************************
+
 #alwys use capital for environment variabls
-echo -e "show me current directory $PWD\n and $UNAME\n"
-#***********************************************************************
+echo  "show me current directory $PWD"
+echo " USER=$USER" 
+echo "system name=$(UNAME -a)"
+
