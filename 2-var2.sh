95a41936d6e9fc712b2f94b0a4fab3a26e031930
jyothi <jyothi3626@gmail.com>
2026-01-28 13:02:44 +0530

    $ declaring var + environ var

---
+++ 2-var2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+#To declare variable and assigning values to it
+name1="jyothi";number=25;course="aws and devops"
+echo "$name1 of age$number taking $course"
+echo "show me current directory$pwd and $path and $uname"
