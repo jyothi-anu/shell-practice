98b44807a89cfbd45932bb094b0369b8f0c50ca5
jyothi <jyothi3626@gmail.com>
2026-01-29 15:02:23 +0530

    reading i/p from user to check condition

--- 13-if_read.sh
+++ 13-if_read.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "enter the number to calculate"
-read num 
-if [ $num -ge 10 ] ; then
+read $1
+if [ $1 -ge 10 ] ; then
 echo "entered num is greater / equal"
 else
 echo "entered num is smaller"
