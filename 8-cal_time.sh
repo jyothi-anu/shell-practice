6cabf69162d6b636ba555fc822f4b9d27547f2fe
jyothi <jyothi3626@gmail.com>
2026-01-29 12:26:27 +0530

    changed spcae

--- 8-cal_time.sh
+++ 8-cal_time.sh
@@ -10,4 +10,4 @@ echo "script ezcuted after excution=$end_time"
 total_time=$(($start_time + $end_time))
 echo "total time taken to run=$total_time"
 total_time1=$(($end_time - $start_time))
-echo "gap btwen start and end time=$total_time1
+echo "gap btwen start and end time=$total_time1"
