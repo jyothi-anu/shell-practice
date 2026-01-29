07f2dfb22a5c9b9a7646692d746d363d1d42cf88
jyothi <jyothi3626@gmail.com>
2026-01-29 15:50:22 +0530

    if root install mysql

--- 14-if_root.sh
+++ 14-if_root.sh
@@ -4,9 +4,11 @@ user=$(id -u)
 if [ $user -ne 0 ]; then
     echo "ur not allowed to install"
     exit 1
+else
+    echo "your allowed to install"
 fi
 
-dnff install mysql -y 
+dnf install mysql -y 
 if [ $? -eq 0 ]; then
     echo "successfully installed mysql"
 else
