702fce0e35daf843e5f2af79270a2607ccc4acbf
jyothi <jyothi3626@gmail.com>
2026-01-29 12:53:41 +0530

    changed spcae

--- 10-datatypes.sh
+++ 10-datatypes.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #########datatypes during excution which type of data it can hold#####
 num=100;num1="heelo"
-sum=$(num+$num)
-str=$(num+$num1)
+sum= $(num+$num)
+str= $(num+$num1)
 
-echo "add of two nums=$sum
-echo "adding add+string=$str
+echo "add of two nums=$sum"
+echo "adding add+string=$str"
