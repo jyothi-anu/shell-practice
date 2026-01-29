4db82f43eeba77407b7a51792fe86d6cd3340436
jyothi <jyothi3626@gmail.com>
2026-01-29 16:03:06 +0530

    if eglible to voot

--- 15-if_age.sh
+++ 15-if_age.sh
@@ -2,7 +2,8 @@
 echo  "enter ur age"
 read age 
 echo "enter ur country"
-if [ $age -ge 18 ] && [ $country -eq "india" ]; then
+read country
+if [ $age -ge 18 ] && [ $country = "india" ]; then
     echo "ur elible to vote in india"
 else 
     echo "ur not eligbw to vote"
