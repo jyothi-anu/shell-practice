cd23c3f6d4198309284cc612d4d854c7e8899905
jyothi <jyothi3626@gmail.com>
2026-01-29 12:10:14 +0530

    storing command into variable

--- 7-dyn_var.sh
+++ 7-dyn_var.sh
@@ -2,3 +2,10 @@
 #########dynamic storing command into variable##########
 time=$(date)
 echo "script excuted at=$time"
+echo "==================================="
+start_time=$(date)
+echo "start time=$start_time"
+sleep 10 
+echo "==================================="
+end_time=$(date)
+echo "end time=$end_time"
